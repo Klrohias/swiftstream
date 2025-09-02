37ed5d714be1f4e91a953a75c6f17be0f2a899a9
qingyiwebt <qingyiweb@gmail.com>
2025-09-03 00:14:09 +0800

    add: fallback proxy

--- swiftstream/src/transfer/proxy_manager.rs
+++ swiftstream/src/transfer/proxy_manager.rs
@@ -5,21 +5,29 @@ use url::ParseError;
 
 pub struct ProxyManager {
     proxies: HashMap<String, Url>,
+    fallback_proxy: Option<Url>,
 }
 
 impl ProxyManager {
     pub fn new() -> Self {
         Self {
             proxies: HashMap::new(),
+            fallback_proxy: None,
         }
     }
 
     pub fn load_proxies(&mut self, input: HashMap<String, String>) -> Result<(), ParseError> {
-        let map = input
+        let mut map = input
             .into_iter()
             .map(|entry| Ok((entry.0, Url::parse(&entry.1)?)))
             .collect::<Result<HashMap<String, Url>, ParseError>>()?;
 
+        // set fallback proxy
+        if let Some(fallback_proxy) = map.remove("fallback") {
+            self.fallback_proxy = Some(fallback_proxy)
+        }
+
+        // insert
         for (k, v) in map.into_iter() {
             self.proxies.insert(k, v);
         }
@@ -34,6 +42,6 @@ impl ProxyManager {
             }
         }
 
-        None
+        self.fallback_proxy.clone()
     }
 }
